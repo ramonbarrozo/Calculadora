cb94d241a7ed37756066b7c797fc1f4983703304
ramonbarrozo <ramoncampos1802@gmail.com>
2026-07-23 13:40:33 -0300

    -segunda versão, em POO.

--- Calculadora.py
+++ Calculadora.py
@@ -1,23 +1,15 @@
-#Entrada de comandos
-print("***Calculadora Simples***")
-print("\nEscolha a operação:")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
+class Calculadora:
+    def __init__(self):
+        self.n1 = float(input("Digite o Nº1: "))
+        self.n2 = float(input("Digite o Nº2: "))
+        self.opcao = input("Escolha a operação: ")
 
-num1 = float(input("Digite o Nº1:"));
-num2 = float(input("Digite o Nº2:"));
-
-opcao = input("Escolha a operação:");
-
-if opcao == "1":
-    print(f"Resultado: {num1+num2}");
-elif opcao == "2":
-    print(f"Resultado: {num1-num2}");
-elif opcao == "3":
-    print(f"Resultado: {num1*num2}");
-elif opcao == "4":
-    print(f"Resultado: {num1/num2}");
-else:
-    print("Opção inválida.");
+    def operacao(self):
+        if self.opcao == "1":
+            return self.n1 + self.n2
+        if self.opcao == "2":
+            return self.n1 - self.n2
+        if self.opcao == "3":
+            return self.n1 * self.n2
+        if self.opcao == "4":
+            return self.n1 / self.n2
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Calculadora Simples Projeto Ramon C. P. Barrozo
 from Calculadora import Calculadora
+
+print("***Calculadora Simples***")
+
+print("\nEscolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão \n")
+
+calc = Calculadora()
+print("Resultado : "+str(calc.operacao()))
